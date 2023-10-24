914bf48ecdf725fca265ad642b021f083af62bb8
DioDiAltro <127589264+DioDiAltro@users.noreply.github.com>
2023-10-24 23:22:58 +0200

    Update Livello3.py

--- MinGW/Livello3/Livello3.py
+++ MinGW/Livello3/Livello3.py
@@ -1,10 +1,8 @@
 import pyautogui as py
 import os
-import ctypes 
-import platform 
+import ctypes
 import pygetwindow as gw
 import cv2
-from PIL import Image
 import numpy as np
 
 def Folder():
@@ -28,35 +26,118 @@ def Folder():
     return folder_path
 
 def Admin():
-    system = platform.system()
-
-    if system == 'Windows':
-        print('Windows,', end=' ')
-
-        try:
-            return ctypes.windll.shell32.IsUserAnAdmin() == 0
+    try:
+        return ctypes.windll.shell32.IsUserAnAdmin() == 0
     
-        except:
-            return False
-        
-    elif system == 'Linux' or system == 'macOS':
-        if system == 'Linux':
-            print('Linux,', end=' ')
+    except:
+        return False
 
-        else:
-            print('macOS,', end=' ')
+def MoveCV(t, Img, Win):
+    py.sleep(t)
+    reference_image_cv2 = cv2.imread(Img)
+    window = gw.getWindowsWithTitle(Win)[0]
 
-        try:
-            return os.geteuid() == 0
+    window.activate()
 
-        except:
-            return False
-        
+    X, Y, W, H = window.left, window.top, window.width, window.height
+
+    while True:
+        # Cattura uno screenshot dello schermo
+        screenshot = py.screenshot(region=(X, Y, W, H))
+
+        # Converte lo screenshot di PIL in un array numpy
+        screenshot_np = np.array(screenshot)
+
+        # Converte l'array numpy in un'immagine OpenCV
+        screenshot_cv2 = cv2.cvtColor(screenshot_np, cv2.COLOR_RGB2BGR)
+
+        # Cerca la posizione dell'immagine di riferimento nello screenshot
+        result = cv2.matchTemplate(screenshot_cv2, reference_image_cv2, cv2.TM_CCOEFF_NORMED)
+        min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
+
+        if max_val > 0.9:  # Puoi regolare la soglia in base alle tue esigenze
+            # La corrispondenza è stata trovata, clicca sulla posizione
+            x, y = max_loc
+            if not Img == 'Gcc.png':
+                py.click(x, y)
+
+            else:
+                py.rightClick(x, y)
+
+            break
+
+def MoveTab(t, x):
+    py.sleep(t)
+    py.press('tab', presses=x)
+    py.press('enter')
+
 if __name__ == '__main__':
     folder_path = Folder()
     url = 'https://sourceforge.net/projects/mingw/'
     username = os.getlogin()
 
-    #Open chrome on MinGW Site
-    os.system(f'start chrome {url}')
-        
+    # #Open chrome on MinGW Site
+    # os.system(f'start chrome {url}')
+    # MoveCV(2, "Livello3\\Download.png", "Google Chrome")
+    
+    # #Wait download MinGW and open
+    # py.sleep(60+5)
+    # os.chdir(f'C:/Users/{username}/Downloads')
+    # os.system('start mingw-get-setup')
+
+    # #Click Install
+    # MoveTab(2, 0)
+
+    # #Check if the User is Admin 
+    # py.sleep(2)
+    # if not Admin():
+    #     #Click change
+    #     MoveTab(2, 3)
+
+    #     #Change dir
+    #     py.press('tab')
+    #     py.sleep(0.5)
+    #     py.hotkey('ctrl', 'a')
+    #     py.sleep(0.5)
+    #     py.press('delete')
+    #     py.sleep(0.5)
+    #     py.typewrite(folder_path)
+    #     py.press('enter')
+
+    # #Click continue
+    # MoveTab(1, 0)
+
+    # #Wait and click continue
+    # MoveTab(120, 0)
+    
+    #MinGW Installer
+    #Clic Gcc
+    MoveCV(2, "Livello3\\Gcc.png", "MinGW Installation Manager")
+
+    #Click Mark for Installation
+    MoveCV(2, "Livello3\\Mark.png", "MinGW Installation Manager")
+
+    #Click Installation 
+    MoveCV(2, "Livello3\\Installation.png", "MinGW Installation Manager")
+
+    #Click Apply Changes
+    MoveCV(2, "Livello3\\Apply.png", "MinGW Installation Manager")
+
+    #Click Apply
+    MoveTab(1, 0)
+
+    #Click Close
+    MoveTab(300, 0)
+
+    #Close MinGW Installer
+    py.hotkey('alt', 'f4')
+
+    if Admin():
+        print(os.system(f'setx PATH "%PATH%; C:\\Users\\{username}\\Desktop\\MinGW\\bin" /M'))
+
+    else:
+        print(os.system(f'setx PATH "%PATH%; C:\\Users\\{username}\\Desktop\\MinGW\\bin"'))
+    
+    print(os.system('g++ --version'))
+
+    #DA PROVARE PASSA PASSO
